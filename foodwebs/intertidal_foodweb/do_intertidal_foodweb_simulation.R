bf2b83ea09e258229ffd165ff6af2eea10974e11
allison barner <allisonkbarner@gmail.com>
2019-04-09 13:30:38 -0700

    finish simulation for larval supply and recruitment var

--- foodwebs/intertidal_foodweb/do_intertidal_foodweb_simulation.R
+++ foodwebs/intertidal_foodweb/do_intertidal_foodweb_simulation.R
@@ -8,7 +8,7 @@ source("foodwebs/intertidal_foodweb/intertidal_foodweb_model_modified.R")
 fd_results <- do.intertidal.simulation()
 
 # plot.new()
-# png(filename = "forde_and_doak_new_dynamics.png", width = 10, height = 7, units = "in", res = 300)
+png(filename = "forde_and_doak_new_dynamics.png", width = 10, height = 7, units = "in", res = 300)
 fd_results %>%
   gather(balanus_glandula:free_space, key = "species",
          value = "abundance") %>%
@@ -18,13 +18,9 @@ fd_results %>%
   geom_line() +
   facet_wrap(~species, scales = "free") +
   theme(legend.position = "none")
-#dev.off()
+dev.off()
 
-
-
-#### Run multiple scenarios ####
-
-## Settlement variation
+#### Run settlement variation scenarios ####
 
 # because settlement rates were roughly estimated, want to run several scenarios under
 # fixed larval supply. 
@@ -34,10 +30,6 @@ fd_results %>%
 # 3. everyone goes down an order of magnitude
 # 4. limpets > barnacles
 
-settlement.B <- .002 * 30 * 24 # to convert into per month
-settlement.C <- .002 * 30 * 24
-settlement.L <- .0002 * 30 * 24
-
 settlement_scenarios <- tribble(
   ~scenario, ~settlement.B, ~settlement.C, ~settlement.L,
   1, .002 * 30 * 24, .002 * 30 * 24, .0002 * 30 * 24,
@@ -46,32 +38,59 @@ settlement_scenarios <- tribble(
   4, .0002 * 30 * 24, .0002 * 30 * 24, .002 * 30 * 24
 )
 
+fd_results_settle_list <- vector(mode = "list", length = 4)
+names(fd_results_settle_list) <- c("main text, forde & doak values",
+                                   "all increased one order of magnitude",
+                                   "all decreased one order of magnitude",
+                                   "forde & doak values, but limpets > barnacles")
 
-## Recruitment variation
+for (i in 1:4) {
+  fd_results_settle_list[[i]] <- do.intertidal.simulation(
+    settlement.B = settlement_scenarios$settlement.B[i],
+    settlement.C = settlement_scenarios$settlement.C[i],
+    settlement.L = settlement_scenarios$settlement.L[i]
+  )
+}
+
+png(filename = "forde_and_doak_larvalsupply_variation.png", width = 12, height = 7, units = "in", res = 300)
+fd_results_settle_list %>%
+  bind_rows(.id = "scenario") %>%
+  # scenario 2 causes system to -> unrealistic free space and barnacle densitites
+  filter(scenario != "settle_2") %>%
+  gather(balanus_glandula:free_space, key = "species",
+         value = "abundance") %>%
+  ggplot(aes(x = time, y = abundance, color = scenario)) +
+  geom_point(size = 2, alpha = 0.7) +
+  geom_line() +
+  facet_wrap(~species, scales = "free")
+dev.off()
+
+#### Run recruitment variation scenarios ####
 
 # want to loop across multiple levels of recruitment variation
 # for each species, from Table 1 in F & D, use "low" variance option each time
-location_function <- function(x_mean, x_stdev) log(x_mean^2 / sqrt(x_stdev^2 + x_mean^2))
-shape_function <- function(x_mean, x_stdev) sqrt(log(1 + (x_stdev ^2 / x_mean^2)))
+
+# see: https://msalganik.wordpress.com/2017/01/21/making-sense-of-the-rlnorm-function-in-r/
+# for how rlnorm function is implemented
 
 larval_supply <- tribble (
   ~species, ~mean_recruit, ~variance_recruit, ~supply_level,
-  "B", 90000, 4.6*10^9, "high",
-  "B", 50000, 1.41*10^10, "med",
-  "B", 6000, 2.025*10^7, "low",
-  "C", 70000, 2.75*10^9, "high",
-  "C", 30000, 5.1*10^8, "med",
-  "C", 6000, 2.025*10^7, "low",
-  "L", 3000, 3.8*10^6, "high",
-  "L", 2400, 2.4*10^6, "med",
-  "L", 200, 169000, "low",
-  "P", 6873, 3.02*10^7, "high",
-  "P", 3800, 9.2*10^6, "med",
-  "P", 727, 3.4*10^5, "low"
+  "B", 90000, sqrt(4.6*10^9), "high",
+  "B", 50000, sqrt(1.41*10^10), "med",
+  "B", 6000, sqrt(2.025*10^7), "low",
+  "C", 70000, sqrt(2.75*10^9), "high",
+  "C", 30000, sqrt(5.1*10^8), "med",
+  "C", 6000, sqrt(2.025*10^7), "low",
+  "L", 3000, sqrt(3.8*10^6), "high",
+  "L", 2400, sqrt(2.4*10^6), "med",
+  "L", 200, sqrt(169000), "low",
+  "P", 6873, sqrt(3.02*10^7), "high",
+  "P", 3800, sqrt(9.2*10^6), "med",
+  "P", 727, sqrt(3.4*10^5), "low"
 )
 
 # which scenarios do we want to run? 
-# B high, C high
+# B high, C high, L high, everyone high
 larval_scenarios_table1 <- tribble (
   ~scenario, ~species, ~supply_level,
   1, "B", "high",
@@ -81,21 +100,50 @@ larval_scenarios_table1 <- tribble (
   2, "B", "low",
   2, "C", "high",
   2, "L", "low",
-  2, "P", "low"
+  2, "P", "low",
+  3, "B", "low",
+  3, "C", "low",
+  3, "L", "high",
+  3, "P", "low",
+  4, "B", "high",
+  4, "C", "high",
+  4, "L", "high",
+  4, "P", "high"
 )
 
 larval_scenarios_table1 %>%
   left_join(larval_supply) %>%
-  mutate(location = location_function(mean_recruit, variance_recruit),
-         shape = shape_function(mean_recruit, variance_recruit)) %>%
-  select(-mean_recruit, -variance_recruit, - supply_level) %>%
-  gather(key = variable, value = value, location, shape) %>%
+  select(-supply_level) %>%
+  gather(key = variable, value = value, mean_recruit, variance_recruit) %>%
   unite(temp, species, variable) %>%
   spread(temp, value) -> larval_scenarios
 
+fd_results_larval_list <- vector(mode = "list", length = 4)
+names(fd_results_larval_list) <- c("B_high", "C_high", "L_high", "all_high")
 
+for (i in 1:4) {
+  fd_results_larval_list[[i]] <- do.intertidal.simulation(
+    B.mean = larval_scenarios$B_mean_recruit[i],
+    B.stdev = larval_scenarios$B_variance_recruit[i],
+    C.mean = larval_scenarios$C_mean_recruit[i],
+    C.stdev = larval_scenarios$C_variance_recruit[i],
+    L.mean = larval_scenarios$L_mean_recruit[i],
+    L.stdev = larval_scenarios$L_variance_recruit[i],
+    P.mean = larval_scenarios$P_mean_recruit[i],
+    P.stdev = larval_scenarios$P_variance_recruit[i]
+  )
+}
 
-
+png(filename = "forde_and_doak_recruitment_variation.png", width = 12, height = 7, units = "in", res = 300)
+fd_results_larval_list %>%
+  bind_rows(.id = "scenario") %>%
+  gather(balanus_glandula:free_space, key = "species",
+         value = "abundance") %>%
+  ggplot(aes(x = time, y = abundance, color = scenario)) +
+  geom_point(size = 2, alpha = 0.7) +
+  geom_line() +
+  facet_wrap(~species, scales = "free")
+dev.off()
 
 
 
